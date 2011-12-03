cfec65097462dba9d45b4afa27d2bc751741a34d
Jonas Oscarsson <jonas.osc@rsson.nu>
2011-12-03 10:26:01 +0100

    Made some changes to imake to make it more convenient (does not clutter source directory with build files anymore).

--- telldusjni.cpp
+++ telldusjni.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <jni.h>
-#include "java/com_homeki_core_device_tellstick_TellStickNative.h"
+#include <com_homeki_core_device_tellstick_TellStickNative.h>
 
 JNIEXPORT void JNICALL 
 Java_com_homeki_core_device_tellstick_TellStickNative_print(JNIEnv *env, jobject obj) {
