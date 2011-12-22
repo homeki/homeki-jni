007fd3160cf21d3c8fac1a0c019d0abfdfb0ed38
Jonas Oscarsson <jonas.osc@rsson.nu>
2011-12-22 23:27:41 +0100

    Moved semaphores from stack to heap.

--- src/tellstickeventqueue.cpp
+++ src/tellstickeventqueue.cpp
@@ -1,27 +1,29 @@
 #include "tellstickeventqueue.h"
 
 TellstickEventQueue::TellstickEventQueue() {
-	sem_init(&sem, 0, 0);
-	sem_init(&mutex, 0, 1);
+	sem = new sem_t();
+	mutex = new sem_t();
+	sem_init(sem, 0, 0);
+	sem_init(mutex, 0, 1);
 }
 
 TellstickEventQueue::~TellstickEventQueue() {
-	sem_destroy(&sem);
-	sem_destroy(&mutex);
+	sem_destroy(sem);
+	sem_destroy(mutex);
 }
 
 void TellstickEventQueue::addEvent(std::string event) {
-	sem_wait(&mutex);
+	sem_wait(mutex);
 	eventQueue.push(event);
-	sem_post(&mutex);
-	sem_post(&sem);
+	sem_post(mutex);
+	sem_post(sem);
 }
 
 std::string TellstickEventQueue::getEvent() {
-	sem_wait(&sem);
-	sem_wait(&mutex);
+	sem_wait(sem);
+	sem_wait(mutex);
 	std::string event = eventQueue.front();
 	eventQueue.pop();
-	sem_post(&mutex);
+	sem_post(mutex);
 	return event;
 }
